2070284e6195ca841b437389e707bc7a489d9bb6
Jack Walker <jwrc@protonmail.com>
2019-07-12 11:26:55 +1000

    Restarted and finished very basic MVP registration form.

--- src/LoginSignup.jsx
+++ src/LoginSignup.jsx
@@ -18,37 +18,28 @@ import { Input } from 'bloomer/lib/elements/Form/Input';
 
 export default function LoginSignUp() {
     return (
-        <Columns isGapless>
-            <Column isSize={{ mobile: 12, default: 8 }} isGapless className="background-primary is-full-vh">
-                <Navbar className="background-primary">
-                    <NavbarBrand className="background-primary">
-                        <span class="font-secondary">Logo</span>
-                    </NavbarBrand>
-                </Navbar>
-                <Column className="has-text-centered">
-                    <Title isSize={3} className="font-secondary">
-                        Welcome
-                    </Title>
-                    <Subtitle isSize={6} className="font-secondary">
-                        Please sign in to continue
-                    </Subtitle>
-                    <Field>
-                        <Label className="font-secondary">Email Address</Label>
-                        <Control>
-                            <Input type="email" placeholder="The place that holds your e-mail" className="font-secondary transparent-background" />
-                        </Control>
-                    </Field>
-                    <Field>
-                        <Label className="font-secondary">Password</Label>
-                        <Control>
-                            <Input type="password" placeholder="We won't watch. Promise (:" className="font-secondary transparent-background fuck-you" />
-                        </Control>
-                    </Field>
-                    <Button isColor="primary" isOutlined className="is-rounded">Submit</Button>
-                </Column>
-            </Column>
-            <Column isSize={{ mobile: 0, default: 4}}>
-            </Column>
-        </Columns>
+       <Column className="has-text-centered big-margin-top">
+            <Title isSize={3} className="font-primary">Welcome!</Title>
+            <Subtitle isSize={6} className="font-primary">Please register to continue</Subtitle>
+            <Field className="small-margin-top">
+                <Label className="font-primary">Email Address</Label>
+                <Control>
+                    <Input type="email" placeholder="The place that stores your e-mail" />
+                </Control>
+            </Field>
+            <Field>
+                <Label className="font-primary">Password</Label>
+                <Control>
+                    <Input type="email" placeholder="We won't look, promise!" />
+                </Control>
+            </Field>
+            <Field>
+                <Label className="font-primary">Confirm Password</Label>
+                <Control>
+                    <Input type="email" placeholder="We won't look, promise!" />
+                </Control>
+            </Field>
+            <Button className="font-primary small-margin-top" isOutlined>Submit</Button>
+       </Column>
     );
 }
