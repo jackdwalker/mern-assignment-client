166f2801297227216685b69ec7a9421541e2e317
Rachel <hello.rachelwong@gmail.com>
2019-07-16 13:06:07 +1000

    view_all_profiles set up

--- src/components/pages/ViewAll.jsx
+++ src/components/pages/ViewAll.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Bulma from 'bulma'
-import { Box, Columns, Container, Column, Select, Content, Title, Button, Pagination, PageControl, PageList, Page, PageEllipsis, PageLink, PanelIcon, PanelBlock, PanelHeading, Panel, Control, Media, MediaContent, MediaLeft, MediaRight, Input, Image, Icon} from "bloomer"
+import { Box, Section, Columns, Container, Column, Select, Content, Title, Button, Pagination, PageControl, PageList, Page, PageEllipsis, PageLink, PanelIcon, PanelBlock, PanelHeading, Panel, Control, Media, MediaContent, MediaLeft, MediaRight, Input, Image, Icon} from "bloomer"
+import "../../profiles.scss"
 
 const ViewAll = () => {
   return (
+    <Section isCentered>
       <Container>
         <Title isSize={3}>View All Profiles</Title>
         <Columns isCentered>
@@ -41,34 +43,127 @@ const ViewAll = () => {
         </Panel>
           </Column>
           <Column isSize="3/4">
-            <Media>
-              <MediaLeft>
-                  <Image className="profileList_image" isSize='128x128' src='https://via.placeholder.com/128x128' />
-              </MediaLeft>
-              <MediaContent>
-                  <Content>
-                    <div>
-                      <Title isSize={4} className="profileList_name">Student Name</Title>
-                      <Title isSize={6} className="profileList_gradType">Fast-Track Graduate</Title>
+            <Box className="profileList_entry">
+              <Media>
+                <MediaLeft>
+                    <Image className="profileList_image" isSize='128x128' src='https://via.placeholder.com/128x128' />
+                </MediaLeft>
+                <MediaContent>
+                    <Content>
+                      <div>
+                        <Title isSize={4} className="profileList_name">Student Name</Title>
+                        <Title isSize={6} className="profileList_gradType">Fast-Track Graduate</Title>
+                      </div>
+                        <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed eros quis est eleifend imperdiet sit amet quis est. Pellentesque id molestie metus. Cras imperdiet ut odio in tristique. Aenean nec lectus velit... (more) </p>
+                    </Content>
+                    <div className="tags are-small profileList_techTags">
+                        <span className="tag is-rounded is-danger profileList_tag">Ruby</span>
+                        <span className="tag is-rounded is-warning">Rails</span>
+                        <span className="tag is-rounded is-light">HTML5</span>
+                        <span className="tag is-rounded is-light">CSS3</span>
+                        <span className="tag is-rounded is-link">Javascript</span>
                     </div>
-                      <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed eros quis est eleifend imperdiet sit amet quis est. Pellentesque id molestie metus. Cras imperdiet ut odio in tristique. Aenean nec lectus velit... (more) </p>
-                  </Content>
-                  <div className="tags are-small profileList_techTags">
-                      <span className="tag is-rounded is-danger profileList_tag">Ruby</span>
-                      <span className="tag is-rounded is-warning">Rails</span>
-                      <span className="tag is-rounded is-light">HTML5</span>
-                      <span className="tag is-rounded is-light">CSS3</span>
-                      <span className="tag is-rounded is-link">Javascript</span>
+                </MediaContent>
+                <MediaRight>
+                  <div className="profileList_jobTypeList">
+                      <Title isSize={6} className="profileList_jobType">Internship</Title>
+                      <Title isSize={6} className="profileList_jobType">Full-time</Title>
+                    </div>
+                  <div className="profileList_viewProfileBttn">
+                    <Button isColor='success'>View Profile</Button>
                   </div>
-              </MediaContent>
-              <MediaRight>
-        <Button isColor='success'>View Profile</Button>
-              </MediaRight>
-            </Media>
+                </MediaRight>
+              </Media>
+            </Box>
+
+
+
+            <Box className="profileList_entry">
+              <Media>
+                <MediaLeft>
+                    <Image className="profileList_image" isSize='128x128' src='https://via.placeholder.com/128x128' />
+                </MediaLeft>
+                <MediaContent>
+                    <Content>
+                      <div>
+                        <Title isSize={4} className="profileList_name">Student Name</Title>
+                        <Title isSize={6} className="profileList_gradType">Fast-Track Graduate</Title>
+                      </div>
+                        <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed eros quis est eleifend imperdiet sit amet quis est. Pellentesque id molestie metus. Cras imperdiet ut odio in tristique. Aenean nec lectus velit... (more) </p>
+                    </Content>
+                    <div className="tags are-small profileList_tagGroup">
+                        <span className="tag is-rounded is-danger profileList_tag">Ruby</span>
+                        <span className="tag is-rounded is-warning profileList_tag">Rails</span>
+                        <span className="tag is-rounded is-light profileList_tag">HTML5</span>
+                        <span className="tag is-rounded is-light profileList_tag">CSS3</span>
+                        <span className="tag is-rounded is-link profileList_tag">Javascript</span>
+                    </div>
+                </MediaContent>
+                <MediaRight>
+                  <div className="profileList_jobTypeList">
+                      <Title isSize={6} className="profileList_jobType">
+                        <Icon className="fas fa-check-circle" />
+                        <span>Internship</span>
+                      </Title>
+                      <Title isSize={6} className="profileList_jobType">
+                        <Icon className="fas fa-check-circle" />
+                        <span>Full-time</span>
+                      </Title>
+                    </div>
+                  <div className="profileList_viewProfileBttn">
+                    <Button isColor='success'>View Profile</Button>
+                  </div>
+                </MediaRight>
+              </Media>
+            </Box>
+
+
+            <Box className="profileList_entry">
+              <Media>
+                <MediaLeft>
+                    <Image className="profileList_image" isSize='128x128' src='https://via.placeholder.com/128x128' />
+                </MediaLeft>
+                <MediaContent>
+                    <Content>
+                      <div>
+                        <Title isSize={4} className="profileList_name">Student Name</Title>
+                        <Title isSize={6} className="profileList_gradType">Fast-Track Graduate</Title>
+                      </div>
+                        <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed eros quis est eleifend imperdiet sit amet quis est. Pellentesque id molestie metus. Cras imperdiet ut odio in tristique. Aenean nec lectus velit... (more) </p>
+                    </Content>
+                    <div className="tags are-small profileList_techTags">
+                        <span className="tag is-rounded is-danger profileList_tag">Ruby</span>
+                        <span className="tag is-rounded is-warning">Rails</span>
+                        <span className="tag is-rounded is-light">HTML5</span>
+                        <span className="tag is-rounded is-light">CSS3</span>
+                        <span className="tag is-rounded is-link">Javascript</span>
+                    </div>
+                </MediaContent>
+                <MediaRight>
+                  <div className="profileList_jobTypeList">
+                      <Title isSize={6} className="profileList_jobType">Internship</Title>
+                      <Title isSize={6} className="profileList_jobType">Full-time</Title>
+                    </div>
+                  <div className="profileList_viewProfileBttn">
+                    <Button isColor='success'>View Profile</Button>
+                  </div>
+                </MediaRight>
+              </Media>
+            </Box>
+
+            <div className="profileList_pagination">
+              <Pagination isAlign="centered">
+                  <PageControl>Previous</PageControl>
+                  <PageControl>Next</PageControl>
+              </Pagination>
+            </div>
           </Column>
         </Columns>
       </Container>
+      </Section>
   )
 }
 
