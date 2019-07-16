781bc105665da3f3a851c1a947533196830144de
Rachel <hello.rachelwong@gmail.com>
2019-07-16 16:09:33 +1000

    page structure profile

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,56 +1,48 @@
 import React from 'react'
 import "../../styles/profile.scss"
-import {Container, Columns, Column, Notification, Section, Box, Image, Icon, Content, Title, Button} from "bloomer"
+import {Container, Columns, Column, Notification, Section, Tag, Box, Image, Icon, Content, Title, Button} from "bloomer"
 
 const Profile = () => {
   return (
     <div>
-          <Section>
-            <Container>
-              <Columns>
-                <Column isSize='1/4' offset="2">
-                    <Title isSize={2}>About this application</Title>
-                </Column>
-                </Columns>
-              </Container>
-            <br/><br/>
 
+      <Section>
       <Columns isCentered>
-
-        <Column isSize='1/4' offset="2">
-                      <Title isSize={2}>About this application</Title>
-
-    <Image isRatio="4:3" src="https://via.placeholder.com/640x480" />
+        <Column isSize='1/4'>
+          <img className="grad_Photo" src="https://picsum.photos/id/290/480/480" alt={""}/>
+          
+          <Container isFluid style={{marginTop: "40px"}}>
+            <Title isSize={5}>Tech Stack</Title>
+          </Container>
         </Column>
-        <Column isSize='1/2'>
+        <Column isSize='1/6'>
             <Content>
-              <Title isSize={2}>Heading 2</Title>
-              <br/>
+        <Title isSize={3}>John Smith</Title>
+
     Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
     <br/><br/>
     Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
     </Content>
-        </Column>
-        </Columns>
-    </Section>
-    <br /><br/>
-    <Section>
-      <Columns isCentered>
-        <Column isSize='1/2'>
-            <Content>
-              <Title isSize={2}>Heading 3</Title>
-              <br/>
-    Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
-    <br/><br/>
-    Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
 
-    </Content>
+                          <Content style={{float:"right"}}>
+        <Button isColor='success' isOutlined>Message Graduate</Button>
+</Content>
+
         </Column>
-        <Column isSize='1/4' offset="2">
-    <Image isRatio="4:3" src="https://via.placeholder.com/640x480" />
+
+        <Column isSize="1/4">
+          <Title isSize={5}>Tech Stack</Title>
+                    <div className="tags are-medium grad_tagGroup">
+                        <span className="tag is-danger profileList_tag">Ruby</span>
+                        <span className="tag is-warning profileList_tag">Rails</span>
+                        <span className="tag is-light profileList_tag">HTML5</span>
+                        <span className="tag is-light profileList_tag">CSS3</span>
+                        <span className="tag is-link profileList_tag">Javascript</span>
+                    </div>
         </Column>
-        
+
         </Columns>
+
     </Section>
 </div>
   )
