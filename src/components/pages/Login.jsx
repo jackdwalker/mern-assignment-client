a510ca00154e63178be884f8500987a7ba29554b
Jack Walker <jwrc@protonmail.com>
2019-07-16 15:52:33 +1000

    Sign-in form now functioning and authenticating users with valid credentials correctly.

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react'
+import { setState } from '../WithState'
 import { Title, Field, Label, Control, Input, Button } from 'bloomer'
+import { api } from '../../studentAPI'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        api.post('/auth/login', {
+            email: this.state.email,
+            password: this.state.password
+        }, {
+            withCredentials: true
+        })        
+        .then(result => {
+            localStorage.setItem('isLoggedIn', true)
+            setState({ isLoggedIn: localStorage.getItem('isLoggedIn') })
+            this.props.history.push('/')
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
     
     render() { 
         return (
@@ -9,25 +42,29 @@ export default class Login extends Component {
                 <div className="loginLeft"></div>
                 <div className="loginRight">
                     <div className="loginBox">
-                        <form>
+                        <form onSubmit={this.handleSubmit}>
                             <Title isSize={4}>Sign into your account</Title>
                             <br />
                             <Field>
                                 <Label>Email</Label>
                                 <Control>
-                                    <Input className="loginInput" isColor='success' placeholder='' value='Your Coder Academy account' />
+                                    <Input className="loginInput" isColor='success' type="email" name="email"
+                                    onChange={this.handleChange} value={this.state.email}
+                                    placeholder="Your Coder Academy email account" />
                                 </Control>
                             </Field>
                             <Field>
                                 <Label>Password</Label>
                                 <Control>
-                                    <Input className="loginInput" isColor='success' type="password" placeholder='' />
+                                    <Input className="loginInput" isColor='success' type="password"  name="password"
+                                    onChange={this.handleChange} value={this.state.password}
+                                    placeholder="We won't look, promise!" />
                                 </Control>
                             </Field>
                             <br />
                             <Field isGrouped>
                                 <Control>
-                                    <Button isColor='primary'>Create Profile</Button>
+                                    <Button isColor='primary' type='submit'>Log In</Button>
                                 </Control>
                             </Field>
                         </form>
