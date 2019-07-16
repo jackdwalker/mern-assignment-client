7047c121531ac03354ae1aa001ac927de38ae574
Rachel <hello.rachelwong@gmail.com>
2019-07-16 14:26:56 +1000

    emergency

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -5,22 +5,22 @@ import {Link} from 'react-router-dom'
 export default class Nav extends Component {
   render() {
     return (
-<Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
-<NavbarBrand>
-    <NavbarItem exact href='/'><Title isSize={3}><Icon isSize="medium" className="fas fa-link"/>  LinkedOut</Title></NavbarItem>
-</NavbarBrand>
-<NavbarMenu isActive={""} onClick={""}>
-    <NavbarStart>
-        <NavbarItem exact href='/'>Home</NavbarItem>
-        <NavbarItem href='/about'>About</NavbarItem>
-        <NavbarItem href='/viewall'>Browse Profiles</NavbarItem>
-    </NavbarStart>
-    <NavbarEnd>
-        <NavbarItem href='/login'>Login</NavbarItem>
-        <NavbarItem href='/signup'>Signup</NavbarItem>
-    </NavbarEnd>
-</NavbarMenu>
-</Navbar>    
-)
+        <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
+        <NavbarBrand>
+            <NavbarItem exact href='/'><Title isSize={3}><Icon isSize="medium" className="fas fa-link"/>  LinkedOut</Title></NavbarItem>
+        </NavbarBrand>
+        <NavbarMenu isActive={""} onClick={""}>
+            <NavbarStart>
+                <NavbarItem exact href='/'>Home</NavbarItem>
+                <NavbarItem href='/about'>About</NavbarItem>
+                <NavbarItem href='/viewall'>Browse Profiles</NavbarItem>
+            </NavbarStart>
+            <NavbarEnd>
+                <NavbarItem href='/login'>Login</NavbarItem>
+                <NavbarItem href='/signup'>Signup</NavbarItem>
+            </NavbarEnd>
+        </NavbarMenu>
+        </Navbar>    
+    )
   }
 }
--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -4,19 +4,55 @@ import {Container, Columns, Column, Notification, Section, Box, Image, Icon, Con
 
 const Profile = () => {
   return (
-    <Container>
+    <div>
+          <Section>
+            <Container>
+              <Columns>
+                <Column isSize='1/4' offset="2">
+                    <Title isSize={2}>About this application</Title>
+                </Column>
+                </Columns>
+              </Container>
+            <br/><br/>
+
       <Columns isCentered>
-    <Column isSize='1/3'>
-        <Notification isColor='success' hasTextAlign='centered'> 1/3 </Notification>
-    </Column>
+
+        <Column isSize='1/4' offset="2">
+                      <Title isSize={2}>About this application</Title>
+
+    <Image isRatio="4:3" src="https://via.placeholder.com/640x480" />
+        </Column>
         <Column isSize='1/2'>
-        <Notification isColor='success' hasTextAlign='centered'> 1/2 </Notification>
-    </Column>
-        <Column isSize='1/4'>
-        <Notification isColor='success' hasTextAlign='centered'> 1/4 </Notification>
-    </Column>
-</Columns>
-</Container>
+            <Content>
+              <Title isSize={2}>Heading 2</Title>
+              <br/>
+    Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
+    <br/><br/>
+    Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
+    </Content>
+        </Column>
+        </Columns>
+    </Section>
+    <br /><br/>
+    <Section>
+      <Columns isCentered>
+        <Column isSize='1/2'>
+            <Content>
+              <Title isSize={2}>Heading 3</Title>
+              <br/>
+    Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
+    <br/><br/>
+    Proin sed tellus sed nunc luctus tristique. Curabitur dapibus risus eget urna ullamcorper posuere. Quisque gravida nunc porttitor pellentesque ultricies. Duis ut molestie metus, in fermentum nulla. Vivamus ac porttitor magna. In ultrices aliquam placerat. Nam mattis nunc turpis.
+
+    </Content>
+        </Column>
+        <Column isSize='1/4' offset="2">
+    <Image isRatio="4:3" src="https://via.placeholder.com/640x480" />
+        </Column>
+        
+        </Columns>
+    </Section>
+</div>
   )
 }
 
