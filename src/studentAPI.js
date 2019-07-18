db4feb7b6c452e79cfd90dc589e95cc450748010
Jack Walker <jwrc@protonmail.com>
2019-07-18 14:54:26 +1000

    The case of the missing cookie has been solved. Sort of.

--- src/studentAPI.js
+++ src/studentAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_EXPRESS_ACCESS_POINT
+    baseURL: process.env.REACT_APP_EXPRESS_ACCESS_POINT || process.env.REACT_APP_TEST_EXPRESS_ACCESS_POINT
 })
