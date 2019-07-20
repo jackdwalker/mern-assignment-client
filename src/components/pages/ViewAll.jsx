3ec2e4c1bb67424c74623063396ed52a7e87d449
Rachel <hello.rachelwong@gmail.com>
2019-07-21 09:53:53 +1000

    signup styling

--- src/components/pages/ViewAll.jsx
+++ src/components/pages/ViewAll.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import Bulma from 'bulma'
 import { Section, Columns, Container, Column, Title, Button, PanelIcon, PanelBlock, PanelHeading, Panel, Control, Input } from "bloomer"
 import "../../styles/viewall.scss"
 import { api } from '../../studentAPI'
 
 import ProfileEntry from "../layout/ProfileEntry"
-import PageIndex from "../layout/PageIndex"
 
 export default class ViewAll extends Component {
   constructor(props) {
@@ -45,45 +43,20 @@ export default class ViewAll extends Component {
           <Title className="profileList_title" isSize={3}>View All Graduate Profiles</Title>
           <Columns isCentered>
             <Column isSize='1/5'>
-              <Panel>
+              <Panel className="searchpanel">
                 <PanelHeading>Search Graduates</PanelHeading>
                 <PanelBlock>
                   <Control hasIcons='left'>
-                    <Input isSize='small' placeholder='Search' />
+                    <Input isSize='small' placeholder='Name, keywords, tech stack' />
                   </Control>
                 </PanelBlock>
-                <PanelBlock isActive>
-                  <PanelIcon className="fas fa-angle-double-right" />
-                  Internships
-            </PanelBlock>
                 <PanelBlock>
-                  <PanelIcon className="fas fa-angle-double-right" />
-                  Remote
-            </PanelBlock>
-                <PanelBlock>
-                  <PanelIcon className="fas fa-angle-double-right" />
-                  Contract
-            </PanelBlock>
-                <PanelBlock>
-                  <PanelIcon className="fas fa-angle-double-right" />
-                  Full-Time
-            </PanelBlock>
-                <PanelBlock>
-                  <PanelIcon className="fas fa-angle-double-right" />
-                  Part-Time
-            </PanelBlock>
-                <PanelBlock>
-                  <Button isOutlined isFullWidth isColor='primary'> Reset Search Filters</Button>
+                  <Button className="searchBttn" isFullWidth isColor='primary'> Search</Button>
                 </PanelBlock>
               </Panel>
             </Column>
             <Column isSize="3/4">
-
-              {/* Profile List Entry */}
               {profileEntry}
-
-              {/* Pagination */}
-              {/* <PageIndex /> */}
             </Column>
           </Columns>
         </Container>
