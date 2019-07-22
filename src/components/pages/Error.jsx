11ea6bb9055201b88bad970e6857f22eddc61732
Rachel <hello.rachelwong@gmail.com>
2019-07-22 13:30:58 +1000

    raph did a 404 page

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import {Columns, Container, Image, Footer, Field, Label,Input, Control, Button, Title, Icon} from 'bloomer'
+import { Container, Image, Title} from 'bloomer'
 import error_image from "../../assets/404_undraw.svg"
 
 const Error = () => {
   return (
     <div>
-      adsfdsfds
       <Container className="error_container">
-        <Title isSize={3}>404 - Sorry no resource</Title>
-        <p>Sorry the page you were looking for doesn't exist.</p>
-        <Image src={error_image} ratio="4:3" alt="404 error"/>
+        <Title className="error_title" isSize={1}>404 - Sorry Not Found</Title>
+        <Title className="error_title" isSize={4}>The resource you were looking for doesn't exist.</Title>
+        <Image src={error_image} className="error_image" alt="404 error"/>
       </Container>    
     </div>
   )
--- src/components/pages/Unauthorised.jsx
+++ src/components/pages/Unauthorised.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import "../../styles/Unauthorised.scss"
-import { Section, Columns, Container, Column, Title, Button, PanelBlock, PanelHeading, Panel, Control, Input, Image } from "bloomer"
+import "../../styles/Error.scss"
+import { Section, Title, Image } from "bloomer"
 import Notify from '../../assets/undraw_notify_88a4.svg'
 
 const Unauthorised = () => {
   return (
     <div>
       <Section className='unauthorised_container'>
-        <Title className='unauthorised_title' isSize={1}>401 unauthorised error</Title>
-        <Title className='unauthorised_title' isSize={4}>unable to view this page</Title>
+        <Title className='unauthorised_title' isSize={1}>401 - Unauthorised error</Title>
+        <Title className='unauthorised_title' isSize={4}>Unable to view this page</Title>
         <Image className='notify_image' src={Notify}/>
       </Section>
 
