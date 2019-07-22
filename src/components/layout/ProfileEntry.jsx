52d3214789473335716a9e4c95f2a225cbdae6fb
Rachel <hello.rachelwong@gmail.com>
2019-07-22 10:47:20 +1000

    re merge

--- src/components/layout/ProfileEntry.jsx
+++ src/components/layout/ProfileEntry.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Box, Content, Title, Button, Media, MediaContent, MediaLeft, MediaRight, Image, Tag, Icon } from "bloomer"
+import Gravatar from 'react-gravatar'
 import "../../styles/profileEntry.scss"
 
-const ProfileEntry = (props) => {
+export default class ProfileEntry extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    let bio
+
+    if (this.props.studentData.bio.length > 244) {
+      bio = `${this.props.studentData.bio.substring(0, 244)}...`
+    } else {
+      bio = this.props.studentData.bio
+    }
+
+    let seeking
+
+    if (this.props.studentData.seeking[0] != '') {
+      seeking = this.props.studentData.seeking.map(seeking =>
+        <Title isSize={6} className="profileList_jobType">
+          <Icon className="fas fa-check-circle" />
+          <span>{seeking}</span>
+        </Title>)
+    } else {
+      seeking = ''
+    }
+
   return (
     <Box className="profileList_entry">
       <Media>
@@ -12,12 +37,12 @@ const ProfileEntry = (props) => {
         <MediaContent>
           <Content className="profileList_content">
             <div>
-              <Title isSize={4} className="profileList_name">{props.studentData.name}</Title>
+              <Title isSize={4} className="profileList_name">{this.props.studentData.name}</Title>
             </div>
-            <p className="profileList_blurb">{props.studentData.bio.substring(0, 244)}...</p>
+            <p className="profileList_blurb">{this.props.studentData.bio.substring(0, 244)}...</p>
           </Content>
           <div className="tags are-small profileList_tagGroup">
-            {props.studentData.techStack.map(language =>
+            {this.props.studentData.techStack.map(language =>
               <Tag className="is-rounded">{language}</Tag>
             )}
           </div>
@@ -25,7 +50,7 @@ const ProfileEntry = (props) => {
         <div className="profileList_right">
           <MediaRight>
             <div className="profileList_jobTypeList">
-              {props.studentData.seeking.map(seeking =>
+              {this.props.studentData.seeking.map(seeking =>
                 <Title isSize={6} className="profileList_jobType">
                   <Icon className="fas fa-check-circle" />
                   <span>{seeking}</span>
@@ -37,11 +62,8 @@ const ProfileEntry = (props) => {
             </div>
           </MediaRight>
         </div>
-
       </Media>
     </Box>
-
   )
 }
-
-export default ProfileEntry
+}
